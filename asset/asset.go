3954131238025d5484075fa6e8df72a1cd4f9582
Ben Lubar <ben.lubar@gmail.com>
2020-09-16 12:08:12 -0500

    enable asset package on JS

--- asset/asset.go
+++ asset/asset.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build darwin linux windows
+// +build darwin linux windows js
 
 package asset
 
